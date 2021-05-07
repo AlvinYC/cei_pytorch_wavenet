24d07684885a32d2461ab42345c632092fa48ad7
AlvinYC <chen.yongcheng@gmail.com>
2021-05-07 09:39:54 +0800

    user GITHUB variable to indicate download folder

--- utils_thisbuild/project_setup.sh
+++ utils_thisbuild/project_setup.sh
@@ -1,16 +1,16 @@
-cd ~/
-cd ~/cei_pytorch_wavenet/utils_thisbuild
+GITHUB="cei_pytorch_wavenet"
+#cd ~/cei_pytorch_wavenet/utils_thisbuild
+cd ~/$GITHUB/utils_thisbuild
 
 # download necessary model file and map file 
 # r9y9 tts prtrain/json
-
 sudo /home/docker/.local/bin/gdown --id "1Z_LsKGIu-xiuFjkCl6SjELzDaVmbxlc0"
 sudo /home/docker/.local/bin/gdown --id "1Qoy6o9BoI0oLjD6TTnn7ErBKMuaXDOp5"
 # cei tts pretrain
 sudo /home/docker/.local/bin/gdown --id "16clV1dTpIBjdZossSDXGnjroe8BjKTzL"
 # r9y9 preset
 sudo /home/docker/.local/bin/gdown --id "1uFBbk0iqyCDAAnrs8jqNgz3T3I9ewuB5"
-sudo chown -R docker:docker ~/cei_pytorch_wavenet/utils_thisbuild
+sudo chown -R docker:docker ~/$GITHUB/utils_thisbuild
 tar -xvf preset.tar.gz
 
 # config git account info for git commit
@@ -18,7 +18,7 @@ sh -c 'git config --global user.name alvinyc'
 sh -c 'git config --global user.email chen.yongcheng@gmail.com'
 
 # move ljspeech inference model to checkpoints path 
-mkdir -p ~/cei_pytorch_wavenet/wavenet_vocoder_2092a64/checkpoints
+mkdir -p ~/$GITHUB/wavenet_vocoder_2092a64/checkpoints
 # necessary nltk data for train/synthesis
 python -c "import nltk; nltk.download('cmudict')"
 python -c "import nltk; nltk.download('punkt')"
